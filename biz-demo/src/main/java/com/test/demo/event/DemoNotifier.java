aed7d91045c76682c754551d6e07459ac407c29b
zhaohan <zhzhaohan@qq.com>
2016-08-03 15:23:41 +0800

    解决事件模型中无法触发事件的问题

--- biz-demo/src/main/java/com/test/demo/event/DemoNotifier.java
+++ biz-demo/src/main/java/com/test/demo/event/DemoNotifier.java
@@ -1,11 +1,14 @@
 package com.test.demo.event;
 
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * Created by zhaohan on 2016/8/3.
  */
+@Component
 public class DemoNotifier {
     @EventListener
     public void processDemoEvent(DemoEvent demoEvent) {
