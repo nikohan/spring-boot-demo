87b641b19194bef3e2608a0fd333ecc372713345
zhaohan <zhzhaohan@qq.com>
2016-08-08 19:02:51 +0800

    update thread's wait and notify

--- biz-demo/src/main/java/com/test/demo/thread/WaitAndNotifyDemo.java
+++ biz-demo/src/main/java/com/test/demo/thread/WaitAndNotifyDemo.java
@@ -6,34 +6,68 @@ package com.test.demo.thread;
 public class WaitAndNotifyDemo {
     public static void main(String[] args) throws InterruptedException {
         Object lock = new Object();
-        Thread t1 = new Thread(new RunnableDemo());
-
+        Thread t1 = new Thread(new WaitDemo(lock));
         t1.start();
-        System.out.println("Thread is starting...");
-
-        Thread.sleep(1000);
-
-        lock.wait();
-        System.out.println("Thread is waiting...");
-
-        Thread.sleep(5000);
-
-        lock.notify();
-        System.out.println("Thread is notified.And the thread continued to run.");
+        System.out.println("thread1 is starting...");
     }
 }
 
-class RunnableDemo implements Runnable {
-    private static int num;
+class WaitDemo implements Runnable {
+    private Object lock;
+    private int num;
+
+    public WaitDemo(Object lock) {
+        this.lock = lock;
+    }
+
     @Override
     public void run() {
         while (num < 100) {
-            try {
-                Thread.sleep(1000);
-                num += 10;
-                System.out.println(num);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            synchronized(lock) {
+                try {
+                    Thread.sleep(1000);
+                    num += 10;
+                    System.out.println("WaitDemo:" + num);
+
+                    if(50 == num) {
+                        Thread t2 = new Thread(new NotifyDemo(lock));
+                        t2.start();
+
+                        System.out.println("thread1 is waiting...");
+                        lock.wait();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
+class NotifyDemo implements Runnable {
+    private int num;
+    private Object lock;
+
+    public NotifyDemo(Object lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        while (num < 100) {
+            synchronized(lock) {
+                try {
+                    Thread.sleep(1000);
+                    num += 10;
+                    System.out.println("NotifyDemo:" + num);
+
+                    if(90 == num) {
+                        lock.notifyAll();
+                        System.out.println("Thread is notified.And the thread continued to run.");
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
