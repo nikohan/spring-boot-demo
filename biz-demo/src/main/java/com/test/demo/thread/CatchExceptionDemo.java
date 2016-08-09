b25302147725cb60bc992e0b9f737acbae6e6b72
zhaohan <zhzhaohan@qq.com>
2016-08-09 15:54:36 +0800

    add catch thread demo

---
+++ biz-demo/src/main/java/com/test/demo/thread/CatchExceptionDemo.java
@@ -0,0 +1,26 @@
+package com.test.demo.thread;
+
+import com.test.demo.domain.User;
+
+/**
+ * Created by zhaohan on 2016/8/9.
+ */
+public class CatchExceptionDemo implements Runnable{
+    User user;
+    public static void main(String[] args) {
+        try {
+            Thread t1 = new Thread(new CatchExceptionDemo());
+            t1.setUncaughtExceptionHandler((Thread t, Throwable e) -> {
+                System.out.println("catch到了!");
+            });
+            t1.start();
+        } catch(Exception e) {
+            System.out.println("catch不到!");
+        }
+    }
+
+    @Override
+    public void run() {
+        System.out.println(user.getName());
+    }
+}
