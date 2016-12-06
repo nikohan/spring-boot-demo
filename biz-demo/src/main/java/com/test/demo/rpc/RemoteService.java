7a88a91eff32f915c654e6f15888da0a3a5d7359
han zhao <han zhao>
2016-12-06 23:43:01 +0800

    add dynamic proxy

---
+++ biz-demo/src/main/java/com/test/demo/rpc/RemoteService.java
@@ -0,0 +1,8 @@
+package com.test.demo.rpc;
+
+/**
+ * Created by nikohan on 2016/12/6.
+ */
+public interface RemoteService {
+    void printHello();
+}
---
+++ biz-demo/src/main/java/com/test/demo/rpc/RpcProxyClient.java
@@ -0,0 +1,38 @@
+package com.test.demo.rpc;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @see <a>http://www.cnblogs.com/LBSer/p/4853234.html</a>
+ * Created by nikohan on 2016/12/6.
+ */
+public class RpcProxyClient implements InvocationHandler{
+
+    private Object target;
+
+    public RpcProxyClient(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        //通信的相关的逻辑写在这里...
+        System.out.println("connecting remote method...");
+        Object result = method.invoke(target, args);
+        return result;
+    }
+
+    //获取被代理的对象
+    public static Object getProxy(Object obj) {
+        return Proxy.newProxyInstance(obj.getClass().getClassLoader(),
+                obj.getClass().getInterfaces(),
+                new RpcProxyClient(obj));
+    }
+
+    public static void main(String[] args) {
+        RemoteService service = (RemoteService) getProxy(new RemoteServiceImpl());
+        service.printHello();
+    }
+}
