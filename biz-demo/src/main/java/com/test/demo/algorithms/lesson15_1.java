fd34607956db51c24ac29b35dd813acf5a04d468
zhaohan <zhzhaohan@qq.com>
2017-01-11 12:08:31 +0800

    add steel bar

---
+++ biz-demo/src/main/java/com/test/demo/algorithms/lesson15_1.java
@@ -0,0 +1,27 @@
+package com.test.demo.algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 钢条切割
+ * Created on 2017/1/10.
+ */
+public class lesson15_1 {
+    private static Map<Integer, Integer> steelBarPriceMap = new HashMap<>();
+
+    static {
+        steelBarPriceMap.put(1, 1);
+        steelBarPriceMap.put(2, 5);
+        steelBarPriceMap.put(3, 8);
+        steelBarPriceMap.put(4, 9);
+        steelBarPriceMap.put(5, 10);
+        steelBarPriceMap.put(6, 17);
+        steelBarPriceMap.put(7, 17);
+        steelBarPriceMap.put(8, 20);
+        steelBarPriceMap.put(9, 24);
+        steelBarPriceMap.put(10, 30);
+    }
+
+
+}
--- biz-demo/src/main/java/com/test/demo/common/PrimaryTypeDemo.java
+++ biz-demo/src/main/java/com/test/demo/common/PrimaryTypeDemo.java
@@ -3,7 +3,7 @@ package com.test.demo.common;
 /**
  * Created by nikohan on 2017/1/4.
  */
-public class PrimaryTypeDedmo {
+public class PrimaryTypeDemo {
     public static void changeLong(Long value) {
         value = new Long(122L);
     }
