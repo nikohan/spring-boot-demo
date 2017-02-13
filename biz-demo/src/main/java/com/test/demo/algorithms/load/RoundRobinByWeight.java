352ec5b8364b0a85805edfc40082cfb048b21b4b
zhaohan <zhzhaohan@qq.com>
2017-02-13 17:47:37 +0800

    add round robin clz

---
+++ biz-demo/src/main/java/com/test/demo/algorithms/load/RoundRobinByWeight.java
@@ -0,0 +1,10 @@
+package com.test.demo.algorithms.load;
+
+/**
+ * 加权轮询
+ * http://blog.csdn.net/gqtcgq/article/details/52076997
+ * Created on 2017/2/13.
+ */
+public class RoundRobinByWeight {
+
+}
