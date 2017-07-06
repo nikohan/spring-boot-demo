24a7a270924f7e33e6d148c6f3037a1792cd7397
zhaohan <han.zhao2@b-and-qchina.com>
2017-07-06 17:09:20 +0800

    add spel

---
+++ biz-demo/src/test/java/com/test/demo/spring/SpELTest.java
@@ -0,0 +1,19 @@
+package com.test.demo.spring;
+
+import org.junit.Test;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+/**
+ * Created on 2017/7/6.
+ */
+public class SpELTest {
+    @Test
+    public void parseExpression() throws Exception {
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression exp = parser.parseExpression("'Hello World'.concat('!')");
+        String message = (String) exp.getValue();
+        System.out.println(message);
+    }
+}
