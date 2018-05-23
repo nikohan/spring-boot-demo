e7829b6950515d900fb56405df1c61906cb776cc
zhaohan <zhaohan@jd.com>
2018-05-23 15:43:01 +0800

    add elastic job for spring boot

---
+++ es-job-demo/src/main/java/com/test/demo/job/JobApplication.java
@@ -0,0 +1,15 @@
+package com.test.demo.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zhaohan on 2018/5/22.
+ */
+@SpringBootApplication
+public class JobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplication.class);
+	}
+}
